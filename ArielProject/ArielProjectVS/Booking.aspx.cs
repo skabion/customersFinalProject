b8a9100ca7de5a10f215b6ac0fd793a1bad73c7a
skabion <arielazari10@gmail.com>
2026-05-04 23:56:10 +0300

    feat():limited table reservation time

--- ArielProject/ArielProjectVS/Booking.aspx.cs
+++ ArielProject/ArielProjectVS/Booking.aspx.cs
@@ -81,8 +81,24 @@ namespace ArielProject
 
             List<TimeSlot> slots = new List<TimeSlot>();
 
-            DateTime startTime = DateTime.Parse("18:00");
-            DateTime endTime = DateTime.Parse("23:30");
+            // קביעת שעות הפעילות לפי המסעדה והיום בשבוע
+            DateTime selectedDate = DateTime.Parse(date);
+            TimeSpan openSpan, closeSpan;
+            GetOpeningHours(res, selectedDate.DayOfWeek, out openSpan, out closeSpan);
+
+            // אפשר להזמין רק עד שעתיים לפני סגירת המסעדה
+            DateTime startTime = DateTime.Today.Add(openSpan);
+            DateTime endTime = DateTime.Today.Add(closeSpan).AddHours(-2);
+
+            if (startTime > endTime)
+            {
+                LblMsg.Text = "המסעדה אינה מקבלת הזמנות בתאריך זה";
+                LblMsg.ForeColor = System.Drawing.Color.Red;
+                RepeaterTimes.DataSource = null;
+                RepeaterTimes.DataBind();
+                RepeaterTimes.Visible = false;
+                return;
+            }
 
             while (startTime <= endTime)
             {
@@ -94,9 +110,132 @@ namespace ArielProject
                 startTime = startTime.AddMinutes(30);
             }
 
+            LblMsg.Text = "";
             RepeaterTimes.DataSource = slots;
             RepeaterTimes.DataBind();
             RepeaterTimes.Visible = true;
+        }
+
+        // מחזירה את שעות הפעילות של המסעדה ביום מסוים בשבוע.
+        // שעות שעוברות חצות מיוצגות עם ערך גדול מ-24 (למשל 25:00 = 01:00 למחרת).
+        // TimeSpan.Zero לפתיחה ולסגירה => המסעדה סגורה ביום זה.
+        private void GetOpeningHours(string res, DayOfWeek day, out TimeSpan open, out TimeSpan close)
+        {
+            // ברירת מחדל - שעות גנריות לכל מסעדה שעוד לא הוגדרה
+            open = new TimeSpan(18, 0, 0);
+            close = new TimeSpan(23, 30, 0);
+
+            bool isFri = day == DayOfWeek.Friday;
+            bool isSat = day == DayOfWeek.Saturday;
+
+            switch (res)
+            {
+                case "Bobo":
+                    // ראשון-חמישי 12:00-23:30 | שישי 12:00-15:00 | שבת 20:00-23:30
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    break;
+
+                case "La Lush":
+                    // ראשון-חמישי 09:00-00:00 | שישי 08:00-16:00 | שבת 19:00-00:00
+                    if (isFri) { open = new TimeSpan(8, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 0, 0); close = new TimeSpan(24, 0, 0); }
+                    else { open = new TimeSpan(9, 0, 0); close = new TimeSpan(24, 0, 0); }
+                    break;
+
+                case "Moses":
+                    // ראשון-רביעי 12:00-00:00 | חמישי-שבת 12:00-01:00
+                    if (day == DayOfWeek.Thursday || isFri || isSat)
+                    { open = new TimeSpan(12, 0, 0); close = new TimeSpan(25, 0, 0); }
+                    else
+                    { open = new TimeSpan(12, 0, 0); close = new TimeSpan(24, 0, 0); }
+                    break;
+
+                case "Japanika":
+                    // ראשון-חמישי 12:00-23:00 | שישי 12:00-15:00 | שבת 19:30-23:00
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 30, 0); close = new TimeSpan(23, 0, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Kagas":
+                    // ראשון-חמישי 10:00-23:00 | שישי 09:00-16:00 | מוצ"ש 20:00-23:30
+                    if (isFri) { open = new TimeSpan(9, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(10, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Vivino":
+                    // ראשון-חמישי 12:00-23:30 | שישי 12:00-16:00 | מוצ"ש 19:00-23:30
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    break;
+
+                case "Mahne Yuda":
+                    // ראשון-חמישי 18:30-01:30 | שישי 12:00-15:30 | שבת סגור
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 30, 0); }
+                    else if (isSat) { open = TimeSpan.Zero; close = TimeSpan.Zero; }
+                    else { open = new TimeSpan(18, 30, 0); close = new TimeSpan(25, 30, 0); }
+                    break;
+
+                case "Oshi Oshi":
+                    // ראשון-חמישי 12:00-23:00 | שישי 11:30-15:00 | מוצ"ש 20:00-23:00
+                    if (isFri) { open = new TimeSpan(11, 30, 0); close = new TimeSpan(15, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Biga":
+                    // ראשון-חמישי 12:00-23:00 | שישי 12:00-15:00 | שבת 19:00-23:00
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Nafis":
+                    // ראשון-חמישי 12:00-23:30 | שישי 12:00-16:00 | מוצ"ש 19:30-23:30
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 30, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    break;
+
+                case "Zink":
+                    // ראשון-חמישי 12:00-23:30 | שישי 12:00-15:30 | מוצ"ש 19:00-00:00
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 30, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 0, 0); close = new TimeSpan(24, 0, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    break;
+
+                case "Max Brener":
+                    // ראשון-חמישי 09:00-23:00 | שישי 09:00-16:00 | מוצ"ש 20:00-23:00
+                    if (isFri) { open = new TimeSpan(9, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    else { open = new TimeSpan(9, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Segev":
+                    // ראשון-חמישי 12:00-23:00 | שישי 12:00-15:00 | מוצ"ש 19:30-23:30
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(15, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(19, 30, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+
+                case "Black":
+                    // ראשון-חמישי 12:00-23:30 | שישי 12:00-16:00 | מוצ"ש 20:00-23:30
+                    if (isFri) { open = new TimeSpan(12, 0, 0); close = new TimeSpan(16, 0, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 30, 0); }
+                    break;
+
+                case "Kansai":
+                    // ראשון-חמישי 12:00-23:00 | שישי 11:30-14:30 | מוצ"ש 20:30-23:00
+                    if (isFri) { open = new TimeSpan(11, 30, 0); close = new TimeSpan(14, 30, 0); }
+                    else if (isSat) { open = new TimeSpan(20, 30, 0); close = new TimeSpan(23, 0, 0); }
+                    else { open = new TimeSpan(12, 0, 0); close = new TimeSpan(23, 0, 0); }
+                    break;
+            }
         }
 
         private bool CheckSpecificTime(string timeToCheck, string res, string date, int total, string type, OleDbConnection con)
